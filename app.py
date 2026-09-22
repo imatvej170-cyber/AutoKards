7eb1282708fa57037baeb5ef6dd9a53f4c8d524f
imatvej170-cyber <imatvej170@gmail.com>
2026-09-22 17:37:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -144,10 +144,10 @@ def get_db():
 def init_db():
     conn = get_db()
     c = conn.cursor()
-    c.execute('''CREATE TABLE IF NOT EXISTS users (
-        id SERIAL PRIMARY KEY, username VARCHAR(20) UNIQUE NOT NULL,
-        password_hash TEXT NOT NULL, created_at TEXT NOT NULL
-    )''')
+  c.execute('''CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY, username VARCHAR(20) UNIQUE NOT NULL,
+  password_hash TEXT NOT NULL, created_at TEXT NOT NULL
+  )''')
     c.execute('ALTER TABLE users ADD COLUMN IF NOT EXISTS avatar_data BYTEA')
     c.execute('ALTER TABLE users ADD COLUMN IF NOT EXISTS avatar_mime TEXT')
     c.execute('ALTER TABLE users ADD COLUMN IF NOT EXISTS bio TEXT')
